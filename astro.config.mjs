94a32f95600b6055c566b6f21dd240f5b0b17be2
Calicolive <calicodeep@gmail.com>
2022-11-17 15:07:55 -0500

    added hamburger menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,6 +43,28 @@ export default defineConfig({
               { name: "buyURL", widget: "string", label: "Post buyURL " },
             ],
           },
+          {
+            name: "freebies",
+            label: "Free Plugins",
+            folder: "src/pages/freebies",
+            create: true,
+            delete: true,
+            fields: [
+              { name: "layout", widget: "string", label: "Post Layout" },
+              { name: "pubDate", widget: "date", label: "Post Date" },
+
+              { name: "title", widget: "string", label: "Post Title" },
+              {
+                name: "description",
+                widget: "markdown",
+                label: "Post description",
+              },
+
+              { name: "heroImage", widget: "image", label: "Post Image" },
+              { name: "body", widget: "markdown", label: "Post Body" },
+              { name: "buyURL", widget: "string", label: "Post buyURL " },
+            ],
+          },
         ],
       },
     }),
